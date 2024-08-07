9abaf96240990b7f751b8aad02e4397acf6f5ba3
Sreehari Sujith <sreehari9300@gmail.com>
2024-08-07 22:00:19 +0530

    added nav

--- src/components/navbar/nav.tsx
+++ src/components/navbar/nav.tsx
@@ -2,6 +2,8 @@
 
 import * as React from "react"
 import Link from "next/link"
+import { Separator } from "@/components/ui/separator"
+
 
 import { cn } from "@/lib/utils"
 import {
@@ -13,6 +15,7 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
+import { List } from "@radix-ui/react-navigation-menu"
 
 // const components: { title: string; href: string; description: string }[] = [
 //   {
@@ -59,10 +62,45 @@ export default function Navbar() {
       <NavigationMenuList>
         <NavigationMenuItem>
           <Link href="/">
-          <NavigationMenuTrigger>Home</NavigationMenuTrigger>
+          Home
           </Link>
           
         </NavigationMenuItem>
+        <Separator orientation="vertical"/>
+        <NavigationMenuItem>
+        <NavigationMenuTrigger>Movies</NavigationMenuTrigger>
+        <NavigationMenuContent>
+        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+            <ListItem
+              key="popular"
+              title="Popular"
+              href="/popular"
+            
+            >
+
+            </ListItem>
+            <ListItem
+              key="theaters"
+              title="Theaters"
+              href="/cinemas"
+            
+            ></ListItem>
+
+
+
+        </ul>
+
+        </NavigationMenuContent>
+        </NavigationMenuItem>
+        <Separator orientation="vertical"/>
+
+        <NavigationMenuItem>
+        <Link href='/accounts'>
+            Account
+            </Link>
+
+        </NavigationMenuItem>
+
         
         
       </NavigationMenuList>
@@ -70,5 +108,31 @@ export default function Navbar() {
     </div>
   )
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
 
 
