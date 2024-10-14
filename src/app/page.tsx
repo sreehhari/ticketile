786574908318cf58461fd23af206b1b9e5fe4bd6
sreehhari <sreehari9300@gmail.com>
2024-10-14 23:42:21 +0530

    added imge ferching  config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,12 @@ export default function Home() {
     <>
     <div className="flex justify-center items-center min-h-screen" >
     <Cards/>
+    <Image
+      src="https://images.unsplash.com/photo-1727873817701-8cd132afbb01?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      width={500}
+      height={500}
+      alt="heisenberg"
+    />
 
     </div>
     
