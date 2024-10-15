0197b0ef51acc64c3d6836de20138ba3a584709b
sreehhari <sreehari9300@gmail.com>
2024-10-15 21:56:46 +0530

    minor changes to image upload

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
       remotePatterns: [
         {
           protocol: 'https',
-          hostname: 'images.unsplash.com',
+          hostname: 'i.pinimg.com',
           port: '',
           pathname: '/**',
         },
